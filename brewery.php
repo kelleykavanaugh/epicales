e15c888daf0a7df436fc670096542e20421aaacf
kelleykavanaugh <kelleykavanaugh@gmail.com>
2013-10-29 16:53:34 -0700

    added query reset on beer, removed <br> replaced with added padding on H1. Adding brewery page and events pages

--- brewery.php
+++ brewery.php
@@ -1,14 +1,19 @@
-    <?php /* Template Name: Beers */
+    <?php /* Template Name: Brewery */
     get_header(); ?>
       <div class="row-fluid">
         <div class="span12"> 
+          <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
           <h1><?php the_title(); ?></h1>
+          <?php the_content() ?>
+            <?php endwhile; ?>
+            <?php endif; ?>
+            <?php wp_reset_query(); ?>
         </div><!--/ .span -->
       </div><!--/ .row -->
       <div class="row-fluid">
         <div class="span12"> 
           <div id="beers-list">
-            <?php query_posts('category_name=beers'); ?>
+            <?php query_posts('category_name=brewery'); ?>
             <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
             <a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
             <?php endwhile; ?>
--- events.php
+++ events.php
@@ -1,26 +1,19 @@
-    <?php /* Template Name: Beers */
+    <?php /* Template Name: Events */
     get_header(); ?>
       <div class="row-fluid">
         <div class="span12"> 
-          <h1><?php the_title(); ?></h1>
-        </div><!--/ .span -->
-      </div><!--/ .row -->
-      <div class="row-fluid">
-        <div class="span12"> 
-          <div id="beers-list">
-            <?php query_posts('category_name=beers'); ?>
-            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-            <a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+          <br>
+          <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+          <?php the_content() ?>
             <?php endwhile; ?>
             <?php endif; ?>
             <?php wp_reset_query(); ?>
-          </div><!--/beers-list-->
-        </div><!--/ span-->
-      </div><!--/row-->
+        </div><!--/ .span -->
+      </div><!--/ .row -->
       <div class="row-fluid">
         <div class="span12">
           <!-- Start The Loop -->
-          <?php query_posts('category_name=beers'); ?>
+          <?php query_posts('category_name=events'); ?>
           <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
               <div id="content" class="white-bg">
                 <h1><?php the_title(); ?></h1>
