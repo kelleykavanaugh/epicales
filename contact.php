4c9ebc1d258f0360d88a0e687677eb7f34037371
kelleykavanaugh <kelleykavanaugh@gmail.com>
2013-10-30 17:32:24 -0700

    fixed contact page

--- contact.php
+++ contact.php
@@ -2,14 +2,14 @@
     get_header(); ?>
       <div class="row-fluid">
         <div class="span12">
-          <!-- Start The Loop -->
-          <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-              <div id="content" class="white-bg">
-                <?php the_content() ?>
-              </div><!--/content-->
-          <?php endwhile; else: ?>
-          <?php endif; ?>
-          <!-- End the Loop -->
+          <div id="content">
+            <!-- Start The Loop -->
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+              <?php the_content() ?>
+            <?php endwhile; else: ?>
+            <?php endif; ?>
+            <!-- End the Loop -->
+          </div><!--/content-->
           </div><!--/ .span -->
         </div><!--/ .row -->
       <?php get_footer(); ?>
