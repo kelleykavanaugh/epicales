dde8c3d7b0b81f55f01edee15fd991303dc3fca9
kelleykavanaugh <kelleykavanaugh@gmail.com>
2013-10-30 23:12:31 -0700

    cleaned up index page, removed superfluous single and page pages

---
+++ news.php
@@ -0,0 +1,33 @@
+    <?php /* Template Name: News */
+    get_header(); ?>
+
+      <div class="row-fluid">
+        <div class="span12"> 
+          <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+          <?php the_content() ?>
+          <?php endwhile; endif; ?>
+          <?php wp_reset_query(); ?>
+        </div><!--/ .span -->
+      </div><!--/ .row -->
+
+      <div class="row-fluid">
+        <div class="span12">
+          <!-- Start The Loop -->
+          <?php query_posts('category_name=news'); ?>
+          <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+              <div id="content" class="white-bg">
+                <div id="<?php the_title_attribute(); ?>"></div>
+                <h1><?php the_title() ?></h1>
+                <div id="center" class="border-bottom">
+                  <?php the_date() ?><br>
+                  <?php the_author() ?>
+                </div>
+                <?php the_content() ?>
+              </div><!--/content-->
+          <?php endwhile; else: ?>
+          <?php endif; ?>
+          <!-- End the Loop -->
+          </div><!--/ .span -->
+        </div><!--/ .row -->
+
+      <?php get_footer(); ?>
